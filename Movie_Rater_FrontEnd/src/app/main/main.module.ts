b3c3839c2c490fa807538cec2e0807c393af303d
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-02-13 20:39:19 +0200

    Added custom router/routes instead of default Angular one

--- Movie_Rater_FrontEnd/src/app/main/main.module.ts
+++ Movie_Rater_FrontEnd/src/app/main/main.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { MainComponent } from './main.component';
 
+const routes: Routes = [
+  {path: 'movies', component: MainComponent}
+];
 
 
 @NgModule({
@@ -9,7 +14,9 @@ import { MainComponent } from './main.component';
     MainComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  exports: [RouterModule],
 })
 export class MainModule { }
